bedc796f03904f0749bd765b8a242631a4d1d5ce
cges1134 <73698355+cges1134@users.noreply.github.com>
2021-01-29 02:21:01 +0800

    Game interface
    21.01.29
    新增標題動畫
    完成C#載入場景

---
+++ 45_2D_GAME/Assets/遊戲介面/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 選單場景的管理器
+/// </summary>
+public class MenuManager : MonoBehaviour
+{
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void StartGame()
+    {
+        // 場景管理 的 載入場景("場景名稱")
+        SceneManager.LoadScene("");
+    }
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        // 應用程式 的 離開遊戲
+        Application.Quit();
+    }
+}
+
